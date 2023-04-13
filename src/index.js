5b0faa9466a4da758b7752f1213627ea9c6d01fb
Cindy Velasquez <ciindvlz@hotmail.com>
2023-04-13 18:52:43 -0400

    MiniPro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   document.querySelector('form').addEventListener('Create New Task', (e) => {
+    e.preventDefault()
+    FixToDo(e.target.new-task-description.value)
+   })
 });
+
+function FixToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo
+  document.querySelector('#tasks').appendChild(p)
+}
